891d17c45b765822f7c377170e639489de95c17e
Miguel Angelo <angelo.gomez1718@gmail.com>
2024-06-04 23:19:34 -0500

    init scaffolding

---
+++ hackatonstudentpoint/src/main/java/com/hackathon/studentpoint/hackatonstudentpoint/HackatonstudentpointApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.studentpoint.hackatonstudentpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonstudentpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonstudentpointApplication.class, args);
+	}
+
+}
