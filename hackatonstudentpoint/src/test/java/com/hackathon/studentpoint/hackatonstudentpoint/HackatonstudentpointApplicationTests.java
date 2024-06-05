602981534910467eab6a698d48ce17c0f2067118
Miguel Angelo <angelo.gomez1718@gmail.com>
2024-06-04 23:10:25 -0500

    scaffolding

---
+++ hackatonstudentpoint/src/test/java/com/hackathon/studentpoint/hackatonstudentpoint/HackatonstudentpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.studentpoint.hackatonstudentpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonstudentpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
